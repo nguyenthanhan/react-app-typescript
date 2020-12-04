bab8995b2b88c3977d73c379e9907ee525ec0fdd
An Nguyen <heimer.nguyen@gmail.com>
2020-12-04 22:16:31 +0700

    #config route of master

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -19,23 +18,29 @@ app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-
+// Priority serve any static files.
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
+// Answer API requests.
 app.use('/api/users', usersRouter);
 
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', function (request, response) {
+  response.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get('env') === 'dev' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
